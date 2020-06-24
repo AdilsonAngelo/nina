9af3b581c7e6869a4ea9b9f7021ba596afb288b4
Adilson Angelo <aasj2@cin.ufpe.br>
2020-06-24 19:33:14 -0300

    aggregator ready

---
+++ aggregator/lambda_function.py
@@ -0,0 +1,35 @@
+import yaml
+import boto3
+
+with open('config.yaml', 'r') as f:
+    CONFIG = yaml.safe_load(f)
+
+QUERY = """
+CREATE TABLE covid_agg AS 
+    (SELECT provincestate,
+         SUM(confirmed) AS confirmed,
+         SUM(recovered) AS recovered,
+         SUM(deaths) AS deaths
+    FROM COVID
+    GROUP BY  provincestate
+    ORDER BY  confirmed DESC)
+"""
+
+ATHENA = boto3.client('athena', aws_access_key_id=CONFIG['aws_access_key_id'],
+                      aws_secret_access_key=CONFIG['aws_secret_access_key'])
+
+
+def lambda_handler(event, context):
+
+    bucket, key = CONFIG['s3agg']['bucket'], CONFIG['s3agg']['key']
+    ATHENA.start_query_execution(
+        QueryString=QUERY,
+        QueryExecutionContext={
+            'Database': CONFIG['database'],
+        },
+        ResultConfiguration={
+            'OutputLocation': f's3://{bucket}/{key}',
+        }
+    )
+
+    print('Done!!')
